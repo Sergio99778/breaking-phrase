b3b32701f897a659c7c32ea8c5a0c9675ebd5287
Sergio99778 <sergio4brandon@gmail.com>
2021-12-04 16:36:47 -0600

    Adding Container to App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import styled from '@emotion/styled';
 
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`;
+
 const Button = styled.button`
   background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
   background-size: 300px;
@@ -14,9 +21,9 @@ const Button = styled.button`
 
 function App() {
   return (
-    <div className="App">
+    <Container>
       <Button>Get Phrase</Button>
-    </div>
+    </Container>
   );
 }
 
